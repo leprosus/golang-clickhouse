4946425a8124a378d2e1a38be7f6fc7cfd23e357
Denis Korolev <admin@leprosus.ru>
2017-03-28 12:24:17 +0200

    Fix: query preparing

--- clickhouse.go
+++ clickhouse.go
@@ -70,6 +70,10 @@ func (conn *Conn) getFQDN() string {
 }
 
 func (conn Conn) doQuery(query string) (io.ReadCloser, error) {
+	//TODO maybe there must be more complex regexp (using will show)
+	re := regexp.MustCompile(";? *$")
+	query = re.ReplaceAllString(query, " FORMAT TabSeparatedWithNames")
+
 	client := http.Client{
 		Timeout: conn.timeout * time.Second}
 
@@ -94,10 +98,6 @@ func (conn Conn) doQuery(query string) (io.ReadCloser, error) {
 }
 
 func (conn *Conn) Fetch(query string) (Iter, error) {
-	//TODO maybe there must be more complex regexp (using will show)
-	re := regexp.MustCompile(";? *$")
-	query = re.ReplaceAllString(query, " FORMAT TabSeparatedWithNames")
-
 	iter := Iter{}
 
 	var err error
