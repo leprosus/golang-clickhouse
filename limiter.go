6cc9d8805bae433bac0378c8232037bf9df4440e
Denis Korolev <admin@leprosus.ru>
2017-11-17 00:12:57 +0200

    Add: lazy init in limiter

--- limiter.go
+++ limiter.go
@@ -21,33 +21,35 @@ func (lim *Limiter) MaxRequests(limit int) {
 
 	message := fmt.Sprintf("Set max request pool = %d", limit)
 	cfg.logger.debug(message)
+}
 
-	lim.once.Do(func() {
-		lim.queue = make(chan int32)
+func (lim *Limiter) initQueue() {
+	lim.queue = make(chan int32)
 
-		go func() {
-			for step := range lim.queue {
-				atomic.AddInt32(&lim.requestsCounter, step)
+	go func() {
+		for step := range lim.queue {
+			atomic.AddInt32(&lim.requestsCounter, step)
 
-				if atomic.LoadUint32(&lim.maxRequests) > 0 {
-					if atomic.LoadUint32(&lim.maxRequests) <= uint32(atomic.LoadInt32(&lim.requestsCounter)) {
-						composer.GetComposer().Pause()
-					} else {
-						composer.GetComposer().Play()
-					}
+			if atomic.LoadUint32(&lim.maxRequests) > 0 {
+				if atomic.LoadUint32(&lim.maxRequests) <= uint32(atomic.LoadInt32(&lim.requestsCounter)) {
+					composer.GetComposer().Pause()
 				} else {
 					composer.GetComposer().Play()
 				}
+			} else {
+				composer.GetComposer().Play()
 			}
-		}()
-	})
+		}
+	}()
 }
 
 func (lim *Limiter) increase() {
+	lim.once.Do(lim.initQueue)
 	lim.queue <- 1
 }
 
 func (lim *Limiter) reduce() {
+	lim.once.Do(lim.initQueue)
 	lim.queue <- -1
 }
 
